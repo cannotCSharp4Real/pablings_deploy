57cef55f3cd7b39208350f8300e8a1224593904b
ninlai <beaninmendevil@gmail.com>
2025-07-09 18:08:28 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 // Use environment variables for database connection (recommended)
-$host = $_ENV['DATABASE_HOST'] ?? 'dpg-dimh2hjesdus73h1ug0-a';
+$host = $_ENV['DATABASE_HOST'] ?? 'dpg-d1n3sv8dl3ps73ft5df0-a';
 $port = $_ENV['DATABASE_PORT'] ?? '5432';
-$dbname = $_ENV['DATABASE_NAME'] ?? 'pablings_dp';
-$username = $_ENV['DATABASE_USER'] ?? 'pablings_dp_user';
-$password = $_ENV['DATABASE_PASSWORD'] ?? 'kwJ2uDyPNRWsQqWqY4uThkgzCagxiuMi';
+$dbname = $_ENV['DATABASE_NAME'] ?? 'pablings';
+$username = $_ENV['DATABASE_USER'] ?? 'pablings_user';
+$password = $_ENV['DATABASE_PASSWORD'] ?? '';
 
 try {
     // Create PDO connection for PostgreSQL

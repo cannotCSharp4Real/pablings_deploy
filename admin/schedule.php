6b9f1e05b3dc0f11e5832769b7937ad83945b01a
ninlai <beaninmendevil@gmail.com>
2025-07-23 14:00:28 +0800

    modified:   admin/schedule.php

--- admin/schedule.php
+++ admin/schedule.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+if(isset($_SESSION["user"])){
+    if(($_SESSION["user"])=="" or $_SESSION['usertype']!='a'){
+        header("location: ../login.php");
+        exit();
+    }
+}else{
+    header("location: ../login.php");
+    exit();
+}
+include("../connection.php");
+$list110 = $database->query("select * from schedule;");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -119,24 +133,9 @@
     </script>
 </head>
 <body>
-    <?php
-    session_start();
-    if(isset($_SESSION["user"])){
-        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='a'){
-            header("location: ../login.php");
-            exit();
-        }
-    }else{
-        header("location: ../login.php");
-        exit();
-    }
     
     
-    //import database
-    include("../connection.php");
-    $list110 = $database->query("select * from schedule;");
     
-    ?>
     <div class="container">
         <div class="menu">
             <table class="menu-container" border="0">

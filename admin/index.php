411cf6452093130d5f2bc6b44525b824d55273d5
ninlai <beaninmendevil@gmail.com>
2025-07-23 17:16:27 +0800

    modified:   admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -135,7 +135,7 @@ include("../connection.php");
                                 </td>
                                 <td style="padding:0px;margin:0px;">
                                     <p class="profile-title">Administrator</p>
-                                    <p class="profile-subtitle">admin@edoc.com</p>
+                                    <p class="profile-subtitle">admin@pablings.com</p>
                                 </td>
                             </tr>
                             <tr>

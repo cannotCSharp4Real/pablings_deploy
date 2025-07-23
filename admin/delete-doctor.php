9cb184ec0ea734abc6a489cd96119d3e839393d8
ninlai <beaninmendevil@gmail.com>
2025-07-23 17:00:42 +0800

    modified:   admin/delete-doctor.php

--- admin/delete-doctor.php
+++ admin/delete-doctor.php
@@ -5,10 +5,12 @@
     if(isset($_SESSION["user"])){
         if(($_SESSION["user"])=="" or $_SESSION['usertype']!='a'){
             header("location: ../login.php");
+            exit();
         }
 
     }else{
         header("location: ../login.php");
+        exit();
     }
     
     
@@ -17,7 +19,7 @@
         include("../connection.php");
         $id=$_GET["id"];
         $result001= $database->query("select * from doctor where docid=$id;");
-        $email=($result001->fetch_assoc())["docemail"];
+        $email=($result001->fetch(PDO::FETCH_ASSOC))["docemail"];
         $sql= $database->query("delete from webuser where email='$email';");
         $sql= $database->query("delete from doctor where docemail='$email';");
         //print_r($email);

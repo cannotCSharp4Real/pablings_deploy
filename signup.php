a2b10b2c663023d01ae73fdf622deb89478610fe
ninlai <beaninmendevil@gmail.com>
2025-07-27 07:14:27 +0800

    signup.php

--- signup.php
+++ signup.php
@@ -1,3 +1,30 @@
+<?php
+//learn from w3schools.com
+//Unset all the server side variables
+
+session_start();
+
+$_SESSION["user"]="";
+$_SESSION["usertype"]="";
+
+// Set the new timezone
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+
+$_SESSION["date"]=$date;
+
+if($_POST){
+    $_SESSION["personal"]=array(
+        'fname'=>$_POST['fname'],
+        'lname'=>$_POST['lname'],
+        'address'=>$_POST['address'],
+        'dob'=>$_POST['dob']
+    );
+
+    print_r($_SESSION["personal"]);
+    header("location: create-account.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,46 +40,6 @@
     
 </head>
 <body>
-<?php
-
-//learn from w3schools.com
-//Unset all the server side variables
-
-session_start();
-
-$_SESSION["user"]="";
-$_SESSION["usertype"]="";
-
-// Set the new timezone
-date_default_timezone_set('Asia/Kolkata');
-$date = date('Y-m-d');
-
-$_SESSION["date"]=$date;
-
-
-
-if($_POST){
-
-    
-
-    $_SESSION["personal"]=array(
-        'fname'=>$_POST['fname'],
-        'lname'=>$_POST['lname'],
-        'address'=>$_POST['address'],
-        'dob'=>$_POST['dob']
-    );
-
-
-    print_r($_SESSION["personal"]);
-    header("location: create-account.php");
-
-
-
-
-}
-
-?>
-
 
     <center>
     <div class="container">

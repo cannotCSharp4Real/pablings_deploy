88c74b648305288ebcd0ccd0224271102e7e5ae2
ninlai <beaninmendevil@gmail.com>
2025-07-26 12:40:51 +0800

    modified:   admin/schedule.php

--- admin/schedule.php
+++ admin/schedule.php
@@ -63,7 +63,7 @@ include("../connection.php");
 </head>
 <body>
     <div class="container">
-        <div class="menu" style="width:200px;min-width:200px;max-width:200px;">
+        <div class="menu">
             <table class="menu-container" border="0">
                 <tr>
                     <td style="padding:10px" colspan="2">
@@ -82,20 +82,20 @@ include("../connection.php");
                                     <a href="../logout.php" ><input type="button" value="Log out" class="logout-btn btn-primary-soft btn"></a>
                                 </td>
                             </tr>
-                        </table>
+                    </table>
                     </td>
                 </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-dashbord" >
-                        <a href="index.php" class="non-style-link-menu"><div><p class="menu-text">Dashboard</p></a></div></a>
+                <tr class="menu-row">
+                    <td class="menu-btn menu-icon-dashbord">
+                        <a href="index.php" class="non-style-link-menu"><div><p class="menu-text">Dashboard</p></div></a>
                     </td>
                 </tr>
                 <tr class="menu-row">
                     <td class="menu-btn menu-icon-barber">
-                        <a href="barber.php" class="non-style-link-menu"><div><p class="menu-text">Barber</p></a></div>
+                        <a href="barber.php" class="non-style-link-menu"><div><p class="menu-text">Barber</p></div></a>
                     </td>
                 </tr>
-                <tr class="menu-row" >
+                <tr class="menu-row">
                     <td class="menu-btn menu-icon-schedule menu-active menu-icon-schedule-active">
                         <a href="schedule.php" class="non-style-link-menu non-style-link-menu-active"><div><p class="menu-text">Schedule</p></div></a>
                     </td>
@@ -105,9 +105,9 @@ include("../connection.php");
                         <a href="appointment.php" class="non-style-link-menu"><div><p class="menu-text">Appointment</p></div></a>
                     </td>
                 </tr>
-                <tr class="menu-row" >
+                <tr class="menu-row">
                     <td class="menu-btn menu-icon-customer">
-                        <a href="customer.php" class="non-style-link-menu"><div><p class="menu-text">Customer</p></a></div>
+                        <a href="customer.php" class="non-style-link-menu"><div><p class="menu-text">Customer</p></div></a>
                     </td>
                 </tr>
             </table>

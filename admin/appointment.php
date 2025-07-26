b1939a0833cacb6cfe856ea2f4f940ebfbb6898b
ninlai <beaninmendevil@gmail.com>
2025-07-26 15:24:21 +0800

    modified:   admin/appointment.php

--- admin/appointment.php
+++ admin/appointment.php
@@ -150,7 +150,7 @@ $list110 = $database->query("select * from appointment;");
                 </tr>
                 <tr class="menu-row" >
                     <td class="menu-btn menu-icon-dashbord" >
-                        <a href="index.php" class="non-style-link-menu"><div><p class="menu-text"><img src="../img/icons/dashboards.svg" style="width: 16px; height: 16px; margin-right: 8px; vertical-align: middle;">Dashboard</p></a></div></a>
+                        <a href="index.php" class="non-style-link-menu"><div><p class="menu-text">Dashboard</p></a></div></a>
                     </td>
                 </tr>
                 <tr class="menu-row">

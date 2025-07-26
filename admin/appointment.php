bae76e34e5352fc204d71aa0df638ee2bbe6af6b
ninlai <beaninmendevil@gmail.com>
2025-07-26 18:26:44 +0800

    modified:   barber/appointment.php

--- admin/appointment.php
+++ admin/appointment.php
@@ -144,23 +144,22 @@ $list110 = $database->query("select * from appointment;");
                                     <a href="../logout.php" ><input type="button" value="Log out" class="logout-btn btn-primary-soft btn"></a>
                                 </td>
                             </tr>
-                    </table>
+                        </table>
                     </td>
-                
                 </tr>
                 <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-dashbord" >
+                    <td class="menu-btn menu-icon-dashbord">
                         <a href="index.php" class="non-style-link-menu"><div><p class="menu-text">Dashboard</p></a></div></a>
                     </td>
                 </tr>
                 <tr class="menu-row">
-                    <td class="menu-btn menu-icon-barber ">
-                        <a href="barber.php" class="non-style-link-menu "><div><p class="menu-text">Barber</p></a></div>
+                    <td class="menu-btn menu-icon-barber">
+                        <a href="barber.php" class="non-style-link-menu"><div><p class="menu-text">Barber</p></a></div>
                     </td>
                 </tr>
                 <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-schedule ">
-                        <a href="schedule.php" class="non-style-link-menu"><div><p class="menu-text">Schedule</p></div></a>
+                    <td class="menu-btn menu-icon-schedule">
+                        <a href="schedule.php" class="non-style-link-menu"><div><p class="menu-text">Schedule</p></a></div>
                     </td>
                 </tr>
                 <tr class="menu-row">
@@ -173,7 +172,6 @@ $list110 = $database->query("select * from appointment;");
                         <a href="customer.php" class="non-style-link-menu"><div><p class="menu-text">Customer</p></a></div>
                     </td>
                 </tr>
-
             </table>
         </div>
         <div class="dash-body">

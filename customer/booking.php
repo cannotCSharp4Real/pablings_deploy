0bf7bb1e97e0fcdfd24b4b088c6434588c97298f
ninlai <beaninmendevil@gmail.com>
2025-07-28 18:24:24 +0800

    modified:   customer/booking.php

--- customer/booking.php
+++ customer/booking.php
@@ -61,6 +61,9 @@ if(isset($_GET["id"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/animations.css">  
+    <link rel="stylesheet" href="../css/main.css">  
+    <link rel="stylesheet" href="../css/admin.css">
     <title>Booking</title>
     <style>
         :root {

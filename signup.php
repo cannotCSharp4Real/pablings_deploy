eaa448f3c97eaefe7d5d1103e694cae3a990cf25
ninlai <beaninmendevil@gmail.com>
2025-07-28 19:07:40 +0800

    modified: signup.php

--- signup.php
+++ signup.php
@@ -35,7 +35,7 @@ if($_POST){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/animations.css">  
     <link rel="stylesheet" href="css/main.css">  
-    <link rel="stylesheet" href="css/signup.css">
+    <link rel="stylesheet" href="css/login.css">
         
     <title>Sign Up</title>
     
@@ -45,72 +45,76 @@ if($_POST){
 
     <center>
     <div class="container">
-        <table border="0">
+        <table border="0" style="margin: 0;padding: 0;width: 60%;">
             <tr>
-                <td colspan="2">
-                    <p class="header-text">Let's Get Started</p>
-                    <p class="sub-text">Add Your Personal Details to Continue</p>
-                </td>
+                <td><p class="header-text">Let's Get Started</p></td>
             </tr>
-            <tr>
-                <form action="" method="POST" >
-                <td class="label-td" colspan="2">
-                    <label for="name" class="form-label">Name: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td">
-                    <input type="text" name="fname" class="input-text" placeholder="First Name" required>
-                </td>
-                <td class="label-td">
-                    <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <label for="address" class="form-label">Address: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="text" name="address" class="input-text" placeholder="Address" required>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <label for="dob" class="form-label">Date of Birth: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="date" name="dob" class="input-text" required>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                </td>
-            </tr>
+            <div class="form-body">
+                <tr>
+                    <td><p class="sub-text">Add Your Personal Details to Continue</p></td>
+                </tr>
+                <tr>
+                    <form action="" method="POST" >
+                    <td class="label-td">
+                        <label for="name" class="form-label">Name: </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label-td">
+                        <input type="text" name="fname" class="input-text" placeholder="First Name" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label-td">
+                        <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label-td">
+                        <label for="address" class="form-label">Address: </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label-td">
+                        <input type="text" name="address" class="input-text" placeholder="Address" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label-td">
+                        <label for="dob" class="form-label">Date of Birth: </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label-td">
+                        <input type="date" name="dob" class="input-text" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label-td">
+                    </td>
+                </tr>
 
-            <tr>
-                <td>
-                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
-                </td>
-                <td>
-                    <input type="submit" value="Next" class="login-btn btn-primary btn">
-                </td>
+                <tr>
+                    <td>
+                        <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Next" class="login-btn btn-primary btn">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <br>
+                        <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
+                        <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                        <br><br><br>
+                    </td>
+                </tr>
 
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <br>
-                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
-                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
-                    <br><br><br>
-                </td>
-            </tr>
-
-                    </form>
-            </tr>
+                        </form>
+            </div>
         </table>
 
     </div>

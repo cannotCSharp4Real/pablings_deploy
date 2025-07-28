0274e2cdb47af19806743eab075374fe885b4c89
ninlai <beaninmendevil@gmail.com>
2025-07-29 07:08:50 +0800

    modified: barber/barber.php

--- barber/barber.php
+++ barber/barber.php
@@ -182,7 +182,7 @@
                         
                         $sqlmain= "select * from barber where docemail='$keyword' or docname='$keyword' or docname like '$keyword%' or docname like '%$keyword' or docname like '%$keyword%'";
                     }else{
-                        $sqlmain= "select * from barber order by docid desc";
+                        $sqlmain= "select * from barber order by id desc";
 
                     }
 
@@ -244,7 +244,7 @@
                                 else{
                                 for ( $x=0; $x<$result->num_rows;$x++){
                                     $row=$result->fetch_assoc();
-                                    $docid=$row["docid"];
+                                    $docid=$row["id"];
                                     $name=$row["docname"];
                                     $email=$row["docemail"];
                                     $spe=$row["specialties"];
@@ -318,7 +318,7 @@
             </div>
             ';
         }elseif($action=='view'){
-            $sqlmain= "select * from barber where docid='$id'";
+            $sqlmain= "select * from barber where id='$id'";
             $result= $database->query($sqlmain);
             $row=$result->fetch_assoc();
             $name=$row["docname"];
@@ -542,7 +542,7 @@
         ';
             }
         }elseif($action=='edit'){
-            $sqlmain= "select * from barber where docid='$id'";
+            $sqlmain= "select * from barber where id='$id'";
             $result= $database->query($sqlmain);
             $row=$result->fetch_assoc();
             $name=$row["docname"];

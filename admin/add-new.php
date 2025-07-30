360ccd8977d0a9fa8e0c7a1cda16381260fd6219
ninlai <beaninmendevil@gmail.com>
2025-07-30 21:05:00 +0800

    Modified: admin/add-new.php

--- admin/add-new.php
+++ admin/add-new.php
@@ -10,6 +10,48 @@ if(isset($_SESSION["user"])){
     exit();
 }
 include("../connection.php");
+
+if($_POST){
+    //print_r($_POST);
+    $result= $database->query("select * from webuser");
+    $name=$_POST['name'];
+    $spec=$_POST['spec'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $cpassword=$_POST['cpassword'];
+    
+    if ($password==$cpassword){
+        $error='3';
+        $result= $database->query("select * from webuser where email='$email';");
+        if($result->rowCount()==1){
+            $error='1';
+        }else{
+
+            $sql1="insert into barber(docemail,docname,docpassword,specialties) values('$email','$name','$password',$spec);";
+            $sql2="insert into webuser(email,usertype) values('$email','d')";
+            $database->query($sql1);
+            $database->query($sql2);
+
+            //echo $sql1;
+            //echo $sql2;
+            $error= '4';
+            
+        }
+        
+    }else{
+        $error='2';
+    }
+
+    
+    
+    
+}else{
+    //header('location: signup.php');
+    $error='3';
+}
+
+
+header("location: barber.php?action=add&error=".$error);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,52 +71,6 @@ include("../connection.php");
 </style>
 </head>
 <body>
-    
-    
-
-    <?php
-    if($_POST){
-        //print_r($_POST);
-        $result= $database->query("select * from webuser");
-        $name=$_POST['name'];
-        $spec=$_POST['spec'];
-        $email=$_POST['email'];
-        $password=$_POST['password'];
-        $cpassword=$_POST['cpassword'];
-        
-        if ($password==$cpassword){
-            $error='3';
-            $result= $database->query("select * from webuser where email='$email';");
-            if($result->rowCount()==1){
-                $error='1';
-            }else{
-
-                $sql1="insert into barber(docemail,docname,docpassword,specialties) values('$email','$name','$password',$spec);";
-                $sql2="insert into webuser values('$email','d')";
-                $database->query($sql1);
-                $database->query($sql2);
-
-                //echo $sql1;
-                //echo $sql2;
-                $error= '4';
-                
-            }
-            
-        }else{
-            $error='2';
-        }
-    
-    
-        
-        
-    }else{
-        //header('location: signup.php');
-        $error='3';
-    }
-    
-
-    header("location: barber.php?action=add&error=".$error);
-    ?>
     
    
 
